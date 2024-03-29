7ee402a66dfda9787c44a9af38e359ce77e4d23e
Fasil hameed <faisalhameed763@gmail.com>
2024-03-29 16:53:40 +0530

    prepare base model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from ChickenDisease import logger
 from ChickenDisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChickenDisease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 
 Stage_name = " Stage Data Ingestion"
@@ -11,3 +13,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
